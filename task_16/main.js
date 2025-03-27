761ebd0f62a0a3aefee8f22f79c51f66c77d5bf1
ShinMoVit <nguyenthehan.dev@gmail.com>
2025-03-27 11:43:09 +0700

    done

--- task_16/main.js
+++ task_16/main.js
@@ -3,7 +3,7 @@ function taxiBill(km) {
   let postage = 0;
   if (isNaN(km) || km <= 0) return `Vui lòng nhập số phù hợp lớn hơn 0`;
   else if (km <= 1) postage = 10000;
-  else if (km < 30) {
+  else if (km <= 30) {
     postage = 10000 + 8000 * (km - 1);
   } else if (km > 30) {
     postage = 10000 + 8000 * 29 + 7000 * (km - 30);
