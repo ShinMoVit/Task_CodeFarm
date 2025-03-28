f605edc8ae2de9be38e2079c00f6383e40406d17
ShinMoVit <nguyenthehan.dev@gmail.com>
2025-03-28 09:32:54 +0700

    change

--- task_17/main.js
+++ task_17/main.js
@@ -1,20 +1,37 @@
-const salary = +prompt("Vui lòng điền thu nhập cá nhân của bạn(đồng): ");
+const salary = +prompt("Vui lòng điền thu nhập cá nhân của bạn (đồng): ");
+
 function taxSalary(salary) {
-  let scot = 0;
+  let tax = 0;
+  const brackets = [
+    { incomeLimit: 11000000, rate: 0 },
+    { incomeLimit: 25000000, rate: 0.05 },
+    { incomeLimit: 50000000, rate: 0.1 },
+    { incomeLimit: 80000000, rate: 0.2 },
+    { incomeLimit: Infinity, rate: 0.3 },
+  ];
+
   if (isNaN(salary) || salary < 0) {
-    return `Thu nhập không hợp lệ`;
-  } else if (salary <= 11000000) {
-    scot = 0;
-  } else if (salary <= 25000000) {
-    scot = salary * 0.05;
-  } else if (salary <= 50000000) {
-    scot = salary * 0.1;
-  } else if (salary <= 80000000) {
-    scot = salary * 0.2;
-  } else if (salary > 80000000) {
-    scot = salary * 0.3;
+    return "Thu nhập không hợp lệ";
   }
-  return `Tiền thuế bạn phải đóng là ${scot.toLocaleString("vi-VN")} đồng`;
+
+  let remainingSalary = salary;
+
+  for (let i = 0; i < brackets.length; i++) {
+    const currentIncomeLimit =
+      i === 0
+        ? brackets[i].incomeLimit
+        : brackets[i].incomeLimit - brackets[i - 1].incomeLimit;
+
+    if (remainingSalary > currentIncomeLimit) {
+      tax += currentIncomeLimit * brackets[i].rate;
+      remainingSalary -= currentIncomeLimit;
+    } else {
+      tax += remainingSalary * brackets[i].rate;
+      break;
+    }
+  }
+
+  return `Tiền thuế bạn phải đóng là ${tax.toLocaleString("vi-VN")} đồng`;
 }
 
 document.writeln(taxSalary(salary));
