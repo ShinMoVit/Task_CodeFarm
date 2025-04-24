7d1d0a2a6b64e301b3ae408fb6059bc807ff7d61
ShinMoVit <nguyenthehan.dev@gmail.com>
2025-04-24 15:43:00 +0700

    change task 37

--- task_37/main.js
+++ task_37/main.js
@@ -1,28 +1,38 @@
 let count = 1;
-const removeItem = document.querySelector(".btn-remove");
+// const removeItem = document.querySelector(".btn-remove");
 const removeAllItem = document.querySelector(".btn-removeAll");
 const addItem = document.querySelector(".btn-add");
 
 addItem.addEventListener("click", () => {
   const list = document.getElementById("list");
+
   const newItem = document.createElement("div");
   newItem.className = "item";
   newItem.textContent = `Phần tử ${count}`;
-  count++;
 
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Xoá";
+  deleteBtn.style.marginLeft = "10px";
+  deleteBtn.addEventListener("click", () => {
+    list.removeChild(newItem);
+  });
+
+  newItem.appendChild(deleteBtn);
   list.appendChild(newItem);
+  count++;
 });
-removeItem.addEventListener("click", () => {
-  const list = document.querySelector("#list");
-  const lastItem = list.lastElementChild;
 
-  if (lastItem) {
-    list.removeChild(lastItem);
-    count = Math.max(1, count - 1);
-  } else {
-    alert("Không còn phần tử nào để xóa!");
-  }
-});
+// removeItem.addEventListener("click", () => {
+//   const list = document.querySelector("#list");
+//   const lastItem = list.lastElementChild;
+
+//   if (lastItem) {
+//     list.removeChild(lastItem);
+//     count = Math.max(1, count - 1);
+//   } else {
+//     alert("Không còn phần tử nào để xóa!");
+//   }
+// });
 
 removeAllItem.addEventListener("click", () => {
   const list = document.querySelector("#list");
@@ -34,7 +44,6 @@ removeAllItem.addEventListener("click", () => {
   const confirmDelete = confirm(
     "Bạn có chắc chắn muốn xóa tất cả phần tử không?"
   );
-
   if (confirmDelete) {
     list.innerHTML = "";
     count = 1;
